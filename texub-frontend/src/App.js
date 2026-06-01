7492a976958e8e879ec58edc1016b299b968e0e5
Geethanjali853 <anjaligeetha760@gmail.com>
2026-06-01 19:08:13 +0530

    sg

---
+++ texub-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Texub Microservices Project</h1>
+      <p>User Service Running on Port 8081</p>
+      <p>Product Service Running on Port 8082</p>
+    </div>
+  );
+}
+
+export default App;
